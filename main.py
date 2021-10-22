c68b07f754ddcc57746c371a5389f9c58d2ca4a1
fullnameddateofbirth <93001028+fullnameddateofbirth@users.noreply.github.com>
2021-10-22 22:04:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,19 @@ mixer.music.set_volume(0.3)
 # Start playing the song
 mixer.music.play()
 
-# infinite loop
-while True:
-		# Waiting For Some Time Randomly
-		timeToWait = random.randint(1,100)
-		time.sleep(timeToWait)
-		# Pausing the music
-		mixer.music.pause()	
-		# Waiting for Three minutes for arrangement of chairs etc
-		time.sleep(5)
-		# Resuming the music
-		mixer.music.unpause()
+# Asking how many people are present.
+number_of_people = int(input("Enter the number of people present."))
+
+i = 0
+# Loop running no. of people present
+while i < number_of_people:
+	# Waiting For Some Time Randomly
+	timeToWait = random.randint(1,100)
+	time.sleep(timeToWait)
+	# Pausing the music
+	mixer.music.pause()	
+	# Waiting for Three minutes for physical activities
+	time.sleep(5)
+	# Resuming the music
+	mixer.music.unpause()
+	i+=1
